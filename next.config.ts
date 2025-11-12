2d7d5769704f875764c031e8ef917b8bc508339c
ether1313 <topnroyal.13@gmail.com>
2025-11-12 15:55:05 +0800

    Fix next.config for Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   typescript: {
-    // ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
 };
 
