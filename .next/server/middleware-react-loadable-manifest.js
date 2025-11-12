4fceef41617ae09e10c8c7ed98e36487572ee010
ether1313 <topnroyal.13@gmail.com>
2025-11-12 15:14:51 +0800

    Add .gitignore for project safety

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components/SecurityGauge.tsx -> react-gauge-component\":{\"id\":\"components/SecurityGauge.tsx -> react-gauge-component\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_react-gauge-component_dist_lib_index_js.js\"]}}"
